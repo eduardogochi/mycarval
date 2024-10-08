f592b03903dc80a75c7f64b315fbec4f189baf5e
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-10-08 13:12:43 -0600

    Add e2e test for signing up as a new user and retrieving the currently logged in user

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -31,4 +31,24 @@ describe('Authentication System', () => {
         expect(response.body.password).not.toEqual('password');
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'abcde@mail.com';
+
+    const response = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        password: 'password',
+      })
+      .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
