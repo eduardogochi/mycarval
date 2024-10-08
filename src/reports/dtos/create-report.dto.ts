1be7ae759ea07152cb50b0b8b21ac3ad1e3285db
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-10-08 13:40:13 -0600

    Added new endpoint to create reports

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,35 @@
+import {
+  IsString,
+  IsNumber,
+  IsLongitude,
+  IsLatitude,
+  Min,
+  Max,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
+  year: number;
+
+  @IsNumber()
+  mileage: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+}
